f70f5917b5bb58dde02fb98a7314031aeab9b51c
Анисов Алексей <anisovas@stud.kai.ru>
2023-04-19 14:30:11 +0300

    Лабораторная работа 5

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,56 @@
 var main = function () {
     "use strict";
+    var headsOfDepartments = [
+        "Галиев Ильфат Рамилович",
+        "Георгиев Дмитрий Сергеевич",
+        "Секачев Герман Дмитриевич"
+    ];
+    var ordinaryEmployees = [
+        "Ибрагимов Тимур Рафаэлевич",
+        "Искандеров Адильхан Нариманович",
+        "Ташлыков Даниил Владимирович",
+        "Миронов Игорь Евгеньевич",
+        "Ямалтдинова Назиля Фанилевна"
+    ];
     $(".tabs a span").toArray().forEach(function (element) {
-        //создаем обработку щелчков для этого элемента
+        var $element = $(element);
         $(element).on("click", function () {
-            //поскольку мы используем версию элемента jQuery,
-            //нужно создать временную переменную,
-            //чтобы избежать постоянного обновления
-            var $element = $(element);
+            var $element = $(element),
+                $content;
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
             if ($element.parent().is(":nth-child(1)")) {
-                console.log("Щелчок на первой вкладке!");
+                $content = $("<ul>");
+                for (var i = 0; i < headsOfDepartments.length; ++i) {
+                    $content.append($("<li>").text(headsOfDepartments[i]));
+                }
+                $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(2)")) {
-                console.log("Щелчок на второй вкладке!");
+                $content = $("<ul>");
+                ordinaryEmployees.forEach(function (employee) {
+                    $content.append($("<li>").text(employee));
+                });
+                $("main .content").append($content);
             } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("Щелчок на третьей вкладке!");
+                $(".content").append("<p>");
+                $(".content").append("<input>");
+				$(".content").append("<button>Добавить</button>");
+				$(".content input").addClass("input_field");
+				$(".content button").addClass("input_button");
+
+                $(".content").on("click", ".input_button", function() {
+                    if ($(".input_field").val() != "") {
+                        ordinaryEmployees.push($(".input_field").val());
+                        alert("Сотрудник был добавлен");
+                    }
+                });
             }
             return false;
-        });
+        })
+        return false;
     });
+    $(".tabs a:first-child span").trigger("click");
 };
 
 main();
