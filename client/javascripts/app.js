7c3b5d8fa9bd676053297a3c664bb2df67bd5cb4
Анисов Алексей <anisovas@stud.kai.ru>
2023-06-07 12:46:35 +0300

    Лабораторная работа 9

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,101 +1,211 @@
-function employeesOrginizedByTags(employeesObjects) {
-    var tags = [];
-    employeesObjects.forEach(function (employee) {
-        employee.tags.forEach(function (tag) {
-            if (tags.indexOf(tag) === -1)
-                tags.push(tag);
-        });
-    });
-    return tags.map(function (tag) {
-        var employeesWithTag = [];
-        employeesObjects.forEach(function (employee) {
-            if (employee.tags.indexOf(tag) !== -1)
-                employeesWithTag.push(employee.name);
-        });
-        return { "tag": tag, "employees": employeesWithTag };
-    });
+function getUrlName() {
+	const paramsString = new URLSearchParams(document.location.href);
+		var searchParams = new URLSearchParams(paramsString);
+		
+		for(var pair of searchParams.entries()) {
+		   urlName = pair[0].replace("http://localhost:3000/users/", "");
+		   urlName = urlName.replace("/", "");
+		   console.log(urlName);
+		   break;
+		}
 }
 
-var main = function (employeesObjects) {
-    "use strict";
-    var $ = jQuery;
-    $(".tabs a span").toArray().forEach(function (element) {
-        var $element = $(element);
-        $(element).on("click", function () {
-
-            var employees = employeesObjects.map(function (employee) {
-                return employee.name;
-            });
-
-            var $element = $(element),
-                $content;
-            $(".tabs a span").removeClass("active");
-            $element.addClass("active");
-            $("main .content").empty();
-            if ($element.parent().is(":nth-child(1)")) {
-                $content = $("<ul>");
-                for (var i = employees.length - 1; i >= 0; --i) {
-                    $content.append($("<li>").text(employees[i]));
-                }
-                $("main .content").append($content);
-            } else if ($element.parent().is(":nth-child(2)")) {
-                $content = $("<ul>");
-                employees.forEach(function (employee) {
-                    $content.append($("<li>").text(employee));
-                });
-                $("main .content").append($content);
-            } else if ($element.parent().is(":nth-child(3)")) {
-                employeesOrginizedByTags(employeesObjects).forEach(function (tag) {
-                    var $tagName = $("<h3>").text(tag.tag),
-                        $content = $("<ul>");
-                    tag.employees.forEach(function (employee) {
-                        var $li = $("<li>").text(employee);
-                        $content.append($li);
-                    });
-                    $("main .content").append($tagName);
-                    $("main .content h3").addClass("tag");
-                    $("main .content").append($content);
-                });
-            } else if ($element.parent().is(":nth-child(4)")) {
-                $(".content").append("<p>");
-                $(".content").append("<h3>Имя:</h3>");
-                $(".content").append("<input id='name'>");
-                $(".content").append("<p>");
-                $(".content").append("<h3>Теги:</h3>");
-                $(".content").append("<input id='tags'>");
-                $(".content").append("<p>");
-                $(".content").append("<button>Добавить</button>");
-                $(".content h3").addClass("label");
-                $(".content input").addClass("input_field");
-                $(".content button").addClass("input_button");
-            }
-            return false;
-        });
-    });
-    $(".content").on("click", ".input_button", function () {
-        var $name = $("#name").val();
-        var $tags = $("#tags").val().split(',');
-
-        if ($name != "") {
-            var newEmployeeObject = {
-                "name": $name,
-                "tags": $tags
-            }
-            $.post("/employees", newEmployeeObject, function () {});
-            $("#name").val("");
-            $("#tags").val("");
-            employeesObjects.push(newEmployeeObject);
-            alert("Сотрудник был добавлен");
-        }
-    });
-    $(".tabs a:first-child span").trigger("click");
+var organizeByTags = function (toDoObjects) { 
+	var tags = []; 
+	toDoObjects.forEach(function (toDo) {
+		toDo.tags.forEach(function (tag) {
+			if (tags.indexOf(tag) === -1) { 
+				tags.push(tag);
+			}
+		});
+	}); 
+	var tagObjects = tags.map(function (tag) {
+		var toDosWithTag = []; 
+		toDoObjects.forEach(function (toDo) {
+			if (toDo.tags.indexOf(tag) !== -1) { 
+				toDosWithTag.push(toDo.description);
+			}
+		});
+		return { "name": tag, "toDos": toDosWithTag };
+	});
+	return tagObjects;
 };
 
+var liaWithEditOrDeleteOnClick = function (todo, callback) {
+	var $todoListItem = $("<li>").text(todo.description),
+		$todoEditLink = $("<a>").attr("href", "clients/" + todo._id),
+		$todoRemoveLink = $("<a>").attr("href", "clients/" + todo._id);
 
+	$todoEditLink.addClass("linkEdit");
+	$todoRemoveLink.addClass("linkRemove");
 
-$(document).ready(function () {
-    $.ajaxSetup({ cache: false });
-    $.getJSON("/employees", function (employeesObjects) {
-        main(employeesObjects);
-    });
+	$todoRemoveLink.text("  Удалить");
+	$todoRemoveLink.on("click", function () {
+		$.ajax({
+			url: "/clients/" + todo._id,
+			type: "DELETE"
+		}).done(function (responde) {
+			callback();
+		}).fail(function (err) {
+			console.log("error on delete 'client'!");
+		});
+		return false;
+	});
+	$todoListItem.append($todoRemoveLink);
+
+	$todoEditLink.text("  Редактировать");
+	$todoEditLink.on("click", function() {
+		var newDescription = prompt(" Введите новое наименование для задачи ", todo.description);
+		if (newDescription !== null && newDescription.trim() !== "") {
+			$.ajax({
+				"url": "/clients/" + todo._id,
+				"type": "PUT",
+				"data": { "description": newDescription },
+			}).done(function (responde) {
+				callback();
+			}).fail(function (err) {
+				console.log("Произошла ошибка: " + err);
+			});
+		}
+		return false;
+	});
+	$todoListItem.append($todoEditLink);
+
+	return $todoListItem;
+}
+
+var main = function (toDoObjects) {
+	"use strict";
+	var tabs = [];
+	tabs.push({
+		"name": "Новые",
+		"content": function(callback) {
+			$.getJSON("http://localhost:3000/clients/" + urlName, function (toDoObjects) {
+				var $content = $("<ul>");
+				for (var i = toDoObjects.length-1; i>=0; i--) {
+					var $todoListItem = liaWithEditOrDeleteOnClick(toDoObjects[i], function() {
+						$(".tabs a:first-child span").trigger("click");
+					});
+					$content.append($todoListItem);
+				}
+				callback(null, $content);
+			}).fail(function (jqXHR, textStatus, error) {
+				callback(error, null);
+			});
+		}
+	});
+
+	tabs.push({
+		"name": "Старые",
+		"content": function(callback) {
+			$.getJSON("http://localhost:3000/clients/" + urlName, function (toDoObjects) {
+				var $content,
+					i;
+				$content = $("<ul>");
+				for (i = 0; i < toDoObjects.length; i++) {
+					var $todoListItem = liaWithEditOrDeleteOnClick(toDoObjects[i], function() {
+						$(".tabs a:nth-child(2) span").trigger("click");
+					});
+					$content.append($todoListItem);
+				}
+				callback(null, $content);
+			}).fail(function(jqXHR, textStatus, error) {
+				callback(error, null);
+			});
+		}
+	});
+
+	tabs.push({
+		"name": "Теги",
+		"content":function (callback) {
+			$.get("http://localhost:3000/clients/" + urlName, function (toDoObjects) {	
+				var organizedByTag = organizeByTags(toDoObjects),
+					$content;
+				organizedByTag.forEach(function (tag) {
+					var $tagName = $("<h3>").text(tag.name);
+						$content = $("<ul>");
+					tag.toDos.forEach(function (description) {
+						var $li = $("<li>").text(description);
+						$content.append($li);
+					});
+					$(".content").append($tagName);
+					$(".content").append($content);
+				});
+				callback(null,$content);
+			}).fail(function (jqXHR, textStatus, error) {
+				callback(error, null);
+			});
+		}
+	});
+
+	tabs.push({
+		"name": " Добавить",
+		"content":function () {
+			$.get("http://localhost:3000/clients/" + urlName, function (toDoObjects) {	
+				var $input = $("<input>"), 
+					$tagInput = $("<input>"),
+					$button = $("<button>").text("Добавить").addClass("button");
+
+				$(".content").append("<p>");
+				$(".content").append($input);
+				$(".content").append("<p>");
+				$(".content").append($tagInput);
+				$(".content").append("<p>");
+				$(".content").append($button);
+				
+				function btnfunc() {
+					var description = $input.val(),
+						tags = $tagInput.val().split(","),
+						newToDo = {"description":description, "tags":tags};
+					$.post("clients", newToDo, function(result) {
+						$input.val("");
+						$tagInput.val("");
+						$(".tabs a:first-child span").trigger("click");
+					});
+				}
+				$button.on("click", function() {
+					btnfunc();
+				});
+				$('.tags').on('keydown',function(e){
+					if (e.which === 13) {
+						btnfunc();
+					}
+				});
+			});
+		}
+	});
+
+	tabs.forEach(function (tab) {
+		var $aElement = $("<a>").attr("href",""),
+			$spanElement = $("<span>").text(tab.name);
+		$aElement.append($spanElement);
+		$(".tabs").append($aElement);
+
+		$spanElement.on("click", function () {
+			var $content;
+			$(".tabs a span").removeClass("active");
+			$spanElement.addClass("active");
+			$(".content").empty();
+			tab.content(function (err, $content) {
+				if (err !== null) {
+					alert ("Возникла проблема при обработке запроса: " + err);
+				} else {
+					$(".content").append($content);
+				}
+			});
+			return false;
+		});
+	});
+
+	$(".tabs a:first-child span").trigger("click");
+}
+
+let urlName = "";
+
+getUrlName();
+
+$(document).ready(function() {
+	$.getJSON("http://localhost:3000/clients/" + urlName, function (toDoObjects) {
+		main(toDoObjects);
+	});
 });
--- server.js
+++ server.js
@@ -1,47 +1,35 @@
-var express = require("express"),
-    http = require("http"),
-    mongoose = require("mongoose")
-    app = express();
+const express = require("express");
+const http = require("http"); 
+const mongoose = require("mongoose");
+const ClientsController = require("./controller/ClientsController.js");
+const UsersController = require("./controller/UsersController.js");
 
+var app = express()
+http.createServer(app).listen(3000);
 app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb://0.0.0.0:27017/egnirc');
 
-var EmployeeSchema = mongoose.Schema({
-    name: String,
-    tags: [String]
-});
-var Employee = mongoose.model("Employee", EmployeeSchema);
-http.createServer(app).listen(3000);
-app.get("/employees", async (req, res) => {
-    await Employee.find()
-        .then(async (Employees) => {
-            res.json(Employees);
+mongoose.connect("mongodb://0.0.0.0:27017/egnirc", { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('db connected...');
         })
-        .catch((err) => {
-            console.log(err);
+        .catch(() => {
+            console.log('bad connection...');
         });
-});
 
-app.post("/employees", async (req, res) => {
-    console.log(req.body);
-    let newEmployee = new Employee({
-        "name": req.body.name,
-        "tags": req.body.tags
-    });
+app.get("/clients", ClientsController.index);
+app.get("/clients/:id", ClientsController.show); 
+app.post("/clients", ClientsController.create);
+app.put("/clients/:id", ClientsController.update);
+app.delete("/clients/:id", ClientsController.destroy);
 
-    await newEmployee.save()
-        .then(async (result) => {
-            await Employee.find()
-                .then(async (result) => {
-                    res.json(result);
-                })
-                .catch(async (err) => {
-                    res.send(err);
-                });
-        })
-        .catch(async (err) => {
-            console.log(err);
-            res.send("ERROR");
-        });
-});
+app.get("/users/:username/clients", ClientsController.index);
+app.post("/users/:username/clients", ClientsController.create);
+app.put("/users/:username/clients/:id", ClientsController.update);
+app.delete("/users/:username/clients/:id", ClientsController.destroy);
+
+app.get("/users", UsersController.index); 
+app.post("/users", UsersController.create); 
+app.get("/users/:username", UsersController.show);
+app.put("/users/:username", UsersController.update);
+app.delete("/users/:username", UsersController.destroy);
